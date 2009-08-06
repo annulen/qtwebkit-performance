8dbaf3ec442e201e79a21f32ebffc8c04a7dadfc
Holger Hans Peter Freyther <zecke@selfish.org>
2009-08-06 09:39:56 +0200

    launcher: Add a simple launcher app to open pages
    e.g. this can be used to run the sunspider benchmark

---
+++ launcher/main.cpp
@@ -0,0 +1,44 @@
+#include <QApplication>
+#include <QWebPage>
+#include <QWebView>
+
+#include <QDebug>
+
+class Monitor : public QObject {
+    Q_OBJECT
+public:
+    Monitor(QWebView* view)
+        : m_view(view)
+    {
+        connect(view, SIGNAL(loadFinished(bool)), SLOT(loadFinished(bool)));
+    }
+
+public Q_SLOTS:
+    void loadFinished(bool result) {
+        qWarning("Load finished with result: %d on '%s'", result, qPrintable(m_view->url().toString()));
+    }
+
+private:
+    QWebView* m_view;
+};
+
+
+int main(int argc, char** argv)
+{
+    QApplication app(argc, argv);
+
+    QWebView* view = new QWebView;
+    Monitor mon(view);
+
+    view->show();
+    view->showFullScreen();
+
+    if (app.arguments().size() >= 2)
+        view->load(QUrl(app.arguments().at(1)));
+    else
+        view->load(QUrl("http://doc.qtsoftware.com"));
+
+    return app.exec();
+}
+
+#include "main.moc"
