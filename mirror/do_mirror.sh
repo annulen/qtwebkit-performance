78bfb2c79a5e0a3f6077cbd78a8565b5ca0faacd
Holger Hans Peter Freyther <zecke@selfish.org>
2009-09-10 09:48:22 +0200

    mirror: Add more websites... some more from the top 50..

--- mirror/do_mirror.sh
+++ mirror/do_mirror.sh
@@ -2,7 +2,7 @@
 # Websites we want to mirror
 
 # English
-WEBSITES="http://us.yahoo.com http://google.com/ncr"
+WEBSITES="http://us.yahoo.com http://google.com/ncr http://maps.google.com http://www.apple.com"
 
 # shopping
 WEBSITES="$WEBSITES http://amazon.com http://ebay.com"
@@ -11,7 +11,12 @@ WEBSITES="$WEBSITES http://amazon.com http://ebay.com"
 WEBSITES="$WEBSITES http://bbc.co.uk http://www.guardian.co.uk http://google.com/news/?hl=en http://news.yahoo.com"
 
 # English entertainment
-WEBSITES="$WEBSITES http://flickr.com http://www.youtube.com/?hl=en_US http://myspace.com http://www.twitter.com"
+WEBSITES="$WEBSITES http://flickr.com http://www.youtube.com/?hl=en_US http://myspace.com http://www.twitter.com http://twitter.com/BARACKOBAMA"
+WEBSITES="$WEBSITES http://reddit.com http://www.facebook.com http://www.gmail.com http://mail.yahoo.com http://www.flickr.com"
+WEBSITES="$WEBSITES http://hi5.com http://www.dailymotion.com"
+
+# From the top50 of webites... well adult content... it exists... no kidding... don't go there if you are legally not allowed
+WEBSITES="$WEBSITES http://youporn.com"
 
 
 # Korean pages
