bec9220f55ced34c8761ed7195d4426d07f0c4fc
Holger Hans Peter Freyther <zecke@selfish.org>
2009-09-30 09:02:29 +0200

    mirror: Use optparse in create_hosts...
    Allow to specify the to be used database on the command line.

--- mirror/create_hosts.py
+++ mirror/create_hosts.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 
-import sqlite3, sys
+import sqlite3, sys, optparse
 
-name = "crawl_db.db"
-connection = sqlite3.connect(name)
+def parse():
+    parser = optparse.OptionParser(version = "Generate /etc/hosts entries",
+                                   usage = "%prog [options] database")
+    parser.add_option("-d", "--db", help = "The crawl database to be used",
+                      action = "store", dest = "db_name", default="crawl_db.db")
+    return parser.parse_args(sys.argv)
+
+(opts, args) = parse()
+connection = sqlite3.connect(opts.db_name)
 
 urls = []
 cursor = connection.execute("SELECT url FROM responses")
@@ -13,5 +20,5 @@ for row in cursor:
         urls.append(url)
 
 for url in urls:
-    print "%s\t%s" % (sys.argv[1], url)
+    print "%s\t%s" % (args[1], url)
 
