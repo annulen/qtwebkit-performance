e0b53d9c865e44b38777eb55abc7472f66ec44cf
Holger Hans Peter Freyther <zecke@selfish.org>
2009-11-22 17:19:41 +0100

    [mirror] Add a utilitiy to get the content of a given file
    Be able to extract one file from the content.

---
+++ host-tools/mirror/get_file.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sqlite3, sys, optparse
+
+def parse():
+    parser = optparse.OptionParser(version = "Print the content of a file to stdout",
+                                   usage = "%prog [options] URL")
+    parser.add_option("-d", "--db", help = "The crawl database to be used",
+                      action = "store", dest = "db_name", default="crawl_db.db")
+    parser.add_option("-o", "--output", help = "Write the file to output",
+                      action = "store", dest = "output", default=None)
+    return parser.parse_args(sys.argv)
+
+(opts, args) = parse()
+connection = sqlite3.connect(opts.db_name)
+
+out = sys.stdout
+if opts.output:
+    out = open(opts.output, "w")
+
+cursor = connection.execute("SELECT data FROM responses WHERE url like ?", [args[1]])
+for row in cursor:
+    print >> out, row[0]
