e0b57d727c1bc8a68c0a9cb2b35e6e3f9df2588d
Holger Hans Peter Freyther <zecke@selfish.org>
2010-01-04 11:36:27 +0100

    [benchmark] Add a new Benchmark class to replace QBENCHMARK
    * We need std deviation and std error in the results and it
      is easier to add this ourselves than to change QBENCHMARK

---
+++ common/benchmark.cc
@@ -0,0 +1,115 @@
+/*
+ * Copyright (C) 2010 Holger Hans Peter Freyther
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE COMPUTER, INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE COMPUTER, INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "benchmark.h"
+
+#include <QDebug>
+
+BenchmarkController::BenchmarkController(const QString& name, Benchmark* parent, int iterations)
+    : i(0)
+    , m_iterations(iterations)
+    , m_benchmark(name)
+    , m_parent(parent)
+{
+    clock_gettime(CLOCK_MONOTONIC, &m_start);
+}
+
+BenchmarkController::~BenchmarkController()
+{
+    m_parent->addBenchmark(m_benchmark);
+}
+
+
+void BenchmarkController::next()
+{
+    m_benchmark.addResult(timeElapsed());
+    ++i;
+    clock_gettime(CLOCK_MONOTONIC, &m_start);
+}
+
+int BenchmarkController::iterations() const
+{
+    return m_iterations;
+}
+
+long long BenchmarkController::timeElapsed() const
+{
+    struct timespec _end;
+    clock_gettime(CLOCK_MONOTONIC, &_end);
+
+    long long start = m_start.tv_sec * 1000000000 + m_start.tv_nsec;
+    long long end = _end.tv_sec * 1000000000 + _end.tv_nsec;
+
+    return qAbs(end - start);
+}
+
+Benchmark::Benchmark()
+    : m_empty(true)
+{}
+
+Benchmark::Benchmark(const QString& name)
+    : m_empty(false)
+    , m_name(name)
+{
+}
+
+QString Benchmark::name() const
+{
+    return m_name;
+}
+
+bool Benchmark::isEmpty() const
+{
+    return m_empty;
+}
+
+void Benchmark::addBenchmark(const Benchmark& benchmark)
+{
+    if (!m_results.isEmpty())
+        qFatal("You can not add benchmarks and results.");
+    m_benchmarks.append(benchmark);
+}
+
+QList<Benchmark> Benchmark::benchmarks() const
+{
+    return m_benchmarks;
+}
+
+void Benchmark::addResult(long long result)
+{
+    if (!m_benchmarks.isEmpty())
+        qFatal("You can not add benchmarks and results.");
+
+    m_results.append(result);
+}
+
+QList<long long> Benchmark::results() const
+{
+    return m_results;
+}
+
+Benchmark* benchmark_parent = new Benchmark("total");
---
+++ common/benchmark.h
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2010 Holger Hans Peter Freyther
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE COMPUTER, INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE COMPUTER, INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef benchmark_h
+#define benchmark_h
+
+#include <QPair>
+#include <QString>
+#include <time.h>
+
+/**
+ * Benchmark related functionality
+ */
+
+class Benchmark {
+public:
+    Benchmark();
+    Benchmark(const QString& name);
+
+    QString name() const;
+    bool isEmpty() const;
+
+    void addBenchmark(const Benchmark&);
+    QList<Benchmark> benchmarks() const;
+
+    void addResult(long long result);
+    QList<long long> results() const;
+
+    QList<QPair<QString, long long> > computedResults();
+
+private:
+    bool m_empty;
+    QString m_name;
+    QList<long long> m_results;
+    QList<Benchmark> m_benchmarks;
+};
+
+class BenchmarkController {
+public:
+    BenchmarkController(const QString& name, Benchmark *parent, int iterations = 10);
+    ~BenchmarkController();
+
+    void next();
+    int iterations() const;
+
+    int i;
+
+private:
+    long long timeElapsed() const;
+    const int m_iterations;
+    Benchmark m_benchmark;
+    Benchmark* m_parent;
+    struct timespec m_start;
+};
+
+void benchmarkOutput(const Benchmark&);
+
+/*
+ * working on the benchmark
+ */
+qreal benchmarkMean(const Benchmark&);
+qreal benchmarkAverage(const Benchmark&);
+qreal benchmarkStdError(const Benchmark&);
+qreal benchmarkStdDeviationUnbiased(const Benchmark&);
+qreal benchmarkStdDeviationBiased(const Benchmark&);
+
+
+/*
+ * Use shadowing
+ */
+extern Benchmark* benchmark_parent;
+
+/*
+ * macros for benchmarking
+ */
+#define WEB_CREATE_GROUP(name) \
+    static Benchmark* old_parent = ::benchmark_parent; \
+    static Benchmark* benchmark_parent = new Benchmark(name); \
+    ::old_parent->add(benchmark_parent);
+
+#define WEB_BENCHMARK(name) \
+    for (BenchmarkController web__controller(name, benchmark_parent); \
+         web__controller.i < web__controller.iterations(); web__controller.next())
+
+#endif
