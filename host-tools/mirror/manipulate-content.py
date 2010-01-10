c4a24c1cc18163f35f452eb30a672165ddb44a9c
Holger Hans Peter Freyther <zecke@selfish.org>
2010-01-10 19:29:01 +0100

    [mirror] Renme script to manipulate-content.py for future updates
    * Rename the script
    * Move updating the Content-Length into a method

--- host-tools/mirror/manipulate-content.py
+++ host-tools/mirror/manipulate-content.py
@@ -18,20 +18,29 @@ def to_num(length):
         return 0
     return int(length)
 
-length = len("content-length: ")
-cur = connection.execute("SELECT url, header, LENGTH(data) from responses")
-for row in cur:
-    old_header = str(row[1])
+def header_content_length(header, new_length):
+    """
+    Update the Content Length
+    """
+    length = len("content-length: ")
+
     start = old_header.find("content-length: ")
     end = old_header.find("\r", start)
 
     old_length = old_header[start + length:end]
-    new_length = to_num(row[2])
-
     if int(old_length) == new_length:
+        return None
+    new_entry = "content-length: %d" % new_length
+    return old_header.replace(old_header[start:end], new_entry, 1)
+
+
+cur = connection.execute("SELECT url, header, LENGTH(data) from responses")
+for row in cur:
+    old_header = str(row[1])
+
+    new_header = header_content_length(old_header, to_num(row[2]))
+    if not new_header:
         continue
 
-    new_entry = "content-length: %d" % new_length
-    new_header = old_header.replace(old_header[start:end], new_entry, 1)
     connection.execute("UPDATE responses SET header = ? WHERE url like ?", [new_header, row[0]])
 connection.commit()
